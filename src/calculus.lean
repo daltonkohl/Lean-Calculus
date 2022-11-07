a86f0f243c99eba7b6e187744be1ce0d9be300df
daltonkohl <79106320+daltonkohl@users.noreply.github.com>
2022-11-07 15:20:53 -0800

    first commit

---
+++ src/calculus.lean
@@ -0,0 +1,16 @@
+/-
+This file will contain lean code proving basic calculus concepts
+Author(s): Dalton Kohl, Jacob Moy, Alex Fant
+
+Goals:
+- Limits
+- Continuity
+- Derivatives
+
+Stretch Goal:
+- Fundamental Theorem of Calculus
+	- Mean Value Theorem
+-/
+
+import data.real.basic
+
